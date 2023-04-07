49bdf5c386329c9b2541c4b3f68d3b616fe1c6a3
Oswaldo Alonzo <oalonzo@gmail.com>
2023-04-06 23:21:45 -0600

    modificando los users.

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,18 +1,44 @@
 import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true,
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Hernández',
+    isFollowing: false,
+  },
+  {
+    userName: 'Pacoweb',
+    name: 'Francisco Juárez',
+    isFollowing: true,
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas Prado',
+    isFollowing: false,
+  },
+];
 
-export function App() {
+export function App () {
   return (
-    <section className="App">
-   <TwitterFollowCard userName='midudev' initialIsFollowing={true}>
-    Miguel Ángel Durán
-   </TwitterFollowCard>
-   <TwitterFollowCard userName='pheralb'>
-    Pablo Hernandez
-   </TwitterFollowCard>
+    <section className='App'>
+      {
+        users.map(({userName, name, isFollowing}) => (
+
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </TwitterFollowCard>
+          ))
+        }
     </section>
   );
 }
-
-
